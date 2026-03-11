a0eb4ae69229f4a042a0be4edfdd7f937bbe911e
sahapat888 <684259052@webmail.npru.ac.th>
2026-03-11 11:09:07 +0700

    injection

---
+++ select_injection_form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>search</title>
+</head>
+
+<body>
+    <h1>Test SQl injectiton</h1>
+    <form action="select_solution.php" method="GET">
+        <input type="text" placeholder="Enter Customer ID" name="CustomerID">
+
+        <br><br>
+        <input type="submit">
+    </form>
+</body>
+
+</html>
---
+++ select_solution.php
@@ -0,0 +1,16 @@
+<?php
+require "connect.php";
+
+$cid = $_GET["CustomerID"];
+$sql = "SELECT * FROM customer where CustomerID = :customerID";
+$stmt = $conn->prepare($sql);
+
+$stmt->bindParam(':customerID', $cid);
+$stmt->execute();
+
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+while ($row = $stmt->fetch()) {
+    echo $row['CustomerID'] . ' ' . $row['Name'] . "<br/>";
+}
+$conn = null;
