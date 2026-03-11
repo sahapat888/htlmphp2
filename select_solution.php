6b9dcd38ff28f131a6c434e54736dcfe4edcaa17
sahapat888 <684259052@webmail.npru.ac.th>
2026-03-11 11:59:32 +0700

    solution2

--- select_solution.php
+++ select_solution.php
@@ -2,7 +2,12 @@
 require "connect.php";
 
 $cid = $_GET["CustomerID"];
-$sql = "SELECT * FROM customer where CustomerID = :customerID";
+$sql = "SELECT c.CustomerID ,c.Name,c.OutstandingDebt,co.CountryName,c.Email
+FROM customer as c
+join country as co on c.CountryCode = co.CountryCode 
+where c.CustomerID =:customerID";
+
+
 $stmt = $conn->prepare($sql);
 
 $stmt->bindParam(':customerID', $cid);
@@ -11,6 +16,6 @@ $stmt->execute();
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
 while ($row = $stmt->fetch()) {
-    echo $row['CustomerID'] . ' ' . $row['Name'] . "<br/>";
+    echo $row['CustomerID'] . ' ' . $row['Name'] . ' ' . $row['OutstandingDebt'] . ' ' . $row['Email'] . ' '  . $row['CountryName'] . "<br/>";
 }
 $conn = null;
